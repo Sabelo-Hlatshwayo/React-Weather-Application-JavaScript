510c19117872d5a90ccf06a00d6a62db3cf8a925
Sabelo <sabelo-hlatshwayo>
2023-01-02 23:32:52 +0200

    Added the content of the Weather component

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,9 +1,23 @@
+import SunnyIcon from "../../assets/icons/SunnyIcon";
 import "./Weather.scss";
 
 function Weather() {
   return (
     <div className="weather">
       <h1 className="weather__heading">the weather app</h1>
+      <div className="weather__content">
+        <span className="weather__temperature">21&#176;</span>
+        <h2 className="weather__location">
+          <span className="weather__location--main">Cape Town</span>
+          <span className="weather__location--sub">
+            23:22 &dash; Monday Jan 2023
+          </span>
+        </h2>
+        <div className="weather__condition">
+          <SunnyIcon className="weather__condition--icon" />
+          <span className="weather__condition--type">Sunny</span>
+        </div>
+      </div>
     </div>
   );
 }
