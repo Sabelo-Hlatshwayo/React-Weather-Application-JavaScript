5aaa87f185ff600d67ebc59650f8cabe10cf2a11
Sabelo <sabelo-hlatshwayo>
2023-01-05 01:27:34 +0200

    Added the form

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -1,7 +1,21 @@
+import SearchIcon from "../../assets/icons/SearchIcon";
 import "./Panel.scss";
 
 function Panel() {
-  return <div className="panel">PANEL COMPONENT</div>;
+  return (
+    <div className="panel">
+      <form className="form">
+        <input
+          type="text"
+          placeholder="Search location..."
+          className="form__input"
+        />
+        <button type="submit" className="form__button">
+          <SearchIcon />
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Panel;
