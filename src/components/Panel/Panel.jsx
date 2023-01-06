7ef0b58bd087f826569cfd4149609fde655a609c
Sabelo <sabelo-hlatshwayo>
2023-01-06 23:40:28 +0200

    Rendered the Locations component

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -1,10 +1,12 @@
 import Form from "../Form/Form";
+import Locations from "../Locations/Locations";
 import "./Panel.scss";
 
 function Panel() {
   return (
     <div className="panel">
       <Form />
+      <Locations />
     </div>
   );
 }
