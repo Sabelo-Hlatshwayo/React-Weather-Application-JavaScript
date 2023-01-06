3bdfdcd2911ec28ec4c746f2d12e1d9ea93b3361
Sabelo <sabelo-hlatshwayo>
2023-01-06 23:52:08 +0200

    Added the overall CSS styles of the Locations component

--- src/components/Locations/Locations.jsx
+++ src/components/Locations/Locations.jsx
@@ -3,10 +3,10 @@ import "./Locations.scss";
 function Locations() {
   return (
     <ul className="locations">
-      <li className="location__location">Cape Town</li>
-      <li className="location__location">Cape Town</li>
-      <li className="location__location">Cape Town</li>
-      <li className="location__location">Cape Town</li>
+      <li className="locations__location">Cape Town</li>
+      <li className="locations__location">Cape Town</li>
+      <li className="locations__location">Cape Town</li>
+      <li className="locations__location">Cape Town</li>
     </ul>
   );
 }
